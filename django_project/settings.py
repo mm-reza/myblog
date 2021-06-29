842e5d0c5f455a5d83c6d3d0ea66f83b82974a09
AgroStat <40055314+AgroStat@users.noreply.github.com>
2021-06-29 09:42:38 -0600

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/uploads/'
+MEDIA_URL = '/media/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 
